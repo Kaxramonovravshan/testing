45f4c257e216c3730e469c17c1b2bbbd5ec12718
Kaxramonovravshan <r17ravshan@gmail.com>
2026-01-30 10:30:27 +0500

    env file

--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -1,17 +1,16 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyApQa6SE-vPZEHIc9i1qCnQmRhKs4hnweA",
-  authDomain: "g-85-fcedb.firebaseapp.com",
-  projectId: "g-85-fcedb",
-  storageBucket: "g-85-fcedb.firebasestorage.app",
-  messagingSenderId: "784836608614",
-  appId: "1:784836608614:web:44865c3379ec66dd9ae3a7",
-  measurementId: "G-SEGC9TJYES",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
